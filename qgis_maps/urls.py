8b7706d12e6e96c8781dd62d48602322c64542bc
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2023-02-27 12:21:00 +0100

    try urls

--- qgis_maps/urls.py
+++ qgis_maps/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     re_path(r'^render-btn/(?P<resource_id>\d+)$', views.render_button),
-    re_path(r'^(?P<sub_dir>.+)/(?P<path>.+\.(html|js|css|jpg|png))$', views.serve_static_file),
-
+    re_path(r'^(?P<sub_dir>.*/)?(?P<path>[^/]+\.[^/]+)$', views.serve_static_file),
 ]
