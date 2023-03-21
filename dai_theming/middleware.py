556752199b4f552b4e1b34d99ee10be35c59e318
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2023-03-21 07:34:03 +0100

    fix group middleware

--- dai_theming/middleware.py
+++ dai_theming/middleware.py
@@ -6,9 +6,9 @@ class CheckUserMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if '/groups/' in request.path or '/people/' in request.path \
+        if ('/groups/' in request.path or '/people/' in request.path) \
                 and not request.user.is_authenticated:
-            return redirect('/account/login/')
+            return redirect('/account/login/?next=/groups')
         response = self.get_response(request)
         return response
 
